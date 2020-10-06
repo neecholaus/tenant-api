dc291ebfe70b4cf4dad896abff9b37497d00ccf2
nick <thisisnickadam@gmail.com>
2020-10-06 15:13:52 -0700

    language compiling and bundling supported

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+let x: string = 'test';
+
+console.log(x);
+
+console.log(typeof x);
+
+interface Tenant {
+    name: string;
+    propertyId: number;
+}
+
+let guy: Tenant = {
+    name: "Nick",
+    propertyId: 1
+};
+
+console.log(guy);
+
+console.log(typeof guy);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+
+    // webpack will take the files from ./src/index
+    entry: './src/index',
+
+    // and output it into /dist as bundle.js
+    output: {
+        path: __dirname,
+        filename: 'server.js'
+    },
+
+    // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+
+    module: {
+        rules: [
+
+            // we use babel-loader to load our js files
+            {
+                test: /\.(ts|js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                },
+            }
+        ]
+    }
+};
