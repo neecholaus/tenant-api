53a9ccc66263f2f97d2f2a3459b85508af93a03d
nick <thisisnickadam@gmail.com>
2020-10-07 00:36:51 -0700

    auth driver uses env values now, added node types

--- src/app/drivers/Auth.ts
+++ src/app/drivers/Auth.ts
@@ -4,13 +4,13 @@ import * as jwt from 'jsonwebtoken';
 
 // Takes an object, generates and returns a JWT token.
 export function generateToken(payload: object): string {
-	return jwt.sign(payload, 'secret');
+	return jwt.sign(payload, process.env.jwt_secret);
 }
 
 // Will attempt to decode and return token, if exception, return throw custom error.
 export function decodeToken(token: string): object {
 	try {
-		return jwt.verify(token, 'secret');
+		return jwt.verify(token, process.env.jwt_secret);
 	} catch (err) {
 		throw new Error('Token was provided but was invalid.');
 	}
