3eac4de7c0e4f971939087cc9f4ed36352041520
nick <thisisnickadam@gmail.com>
2020-10-06 21:59:10 -0700

    stupid simple token gen/decode in global middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 import express from 'express';
+import * as jwt from 'jsonwebtoken';
 
 import * as http from './app/resources/http';
 
 const app = express();
+
+app.use((req, res, next) => {
+	if (req.headers.authorization) {
+		let bearerToken = req.headers.authorization.split(' ')[1];
+
+		try {
+			let decoded = jwt.verify(bearerToken, 'shhhs');
+			console.log('authorized', decoded);
+		} catch (err) {
+			console.log('invalid token');
+		}
+	} else {
+		let token = jwt.sign({foo: 'bar'}, 'shhhssdd');
+		console.log('not authorized', token);
+	}
+
+	next();
+});
 
 app.get('/', (req, res) => {
 	res.status(400).send(<http.Response> {
