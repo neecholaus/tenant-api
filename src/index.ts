bedebcdcb4f8b089ef9391b75fd0f554e15660bd
nick <thisisnickadam@gmail.com>
2020-10-06 23:12:59 -0700

    removed temp code from server entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
 import express from 'express';
 const app = express();
 
-// framework interfaces
-// import * as http from './app/resources/http';
-
-// middleware
-// import {Authenticate} from './app/middleware/authenticate';
-
 // routers
 import {authRouter} from './app/routers/authRouter';
-
-// temp demo of auth
-// app.use(Authenticate.handle);
 
 // auth module
 app.use('/auth', authRouter)
