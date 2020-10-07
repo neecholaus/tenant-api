52924fe7bc620cc7fe6d534caf2e3ee61b9cb327
nick <thisisnickadam@gmail.com>
2020-10-07 12:05:19 -0700

    adjusted documentation, removed todo

--- src/app/drivers/Auth.ts
+++ src/app/drivers/Auth.ts
@@ -1,13 +1,11 @@
 import * as jwt from 'jsonwebtoken';
 
-// TODO - move secret to env file
-
 // Takes an object, generates and returns a JWT token.
 export function generateToken(payload: object): string {
 	return jwt.sign(payload, process.env.jwt_secret);
 }
 
-// Will attempt to decode and return token, if exception, return throw custom error.
+// Will attempt to decode and return token, if exception, throw custom error.
 export function decodeToken(token: string): object {
 	try {
 		return jwt.verify(token, process.env.jwt_secret);
