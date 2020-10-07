ab94b6e654ec776ae872c78308720d629fbb5de6
nick <thisisnickadam@gmail.com>
2020-10-06 23:29:08 -0700

    env values are now supported

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 const app = express();
 
+// support env variables
+import * as dotenv from 'dotenv';
+dotenv.config()
+
 // routers
 import {authRouter} from './app/routers/authRouter';
 
