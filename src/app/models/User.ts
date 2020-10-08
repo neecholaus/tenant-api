551d31507cf1c51f8be2d30f8af295c62dc470a4
nick <thisisnickadam@gmail.com>
2020-10-08 02:32:56 -0700

    added fields to user schema for password reset

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -7,6 +7,8 @@ interface IUser extends mongoose.Document {
 	password: string;
 	firstName: string;
 	lastName: string;
+	passwordResetToken: string;
+	passwordResetTokenExp: Date;
 	createdAt: Date;
 	updatedAt: Date;
 }
@@ -17,6 +19,8 @@ const UserSchema = new mongoose.Schema({
 	firstName: String,
 	lastName: String,
 	password: String,
+	passwordResetToken: String,
+	passwordResetTokenExp: Date,
 	createdAt: Date,
 	updatedAt: Date
 });
