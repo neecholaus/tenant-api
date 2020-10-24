258efa7b2125936bda4d7d1c6c174b5a5dd0b7ff
nick <thisisnickadam@gmail.com>
2020-10-24 15:40:04 -0700

    added test for whoami route

--- tests/controllers/UserController.spec.ts
+++ tests/controllers/UserController.spec.ts
@@ -3,5 +3,11 @@ import {Response} from 'supertest';
 import app from '../../src/app';
 
 describe("Test UserController", () => {
-	//
+	test('whoami should return 403', () => {
+		return request(app)
+			.get('/auth/whoami')
+			.then((res: Response) => {
+				expect(res.status).toBe(403);
+			});
+	});
 });
