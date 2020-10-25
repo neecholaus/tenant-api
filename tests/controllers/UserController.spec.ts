17c93688e826855fe1ebfd79b743b2e446b8ee26
nick <thisisnickadam@gmail.com>
2020-10-24 22:46:12 -0700

    added test for whoami with jwt token

--- tests/controllers/UserController.spec.ts
+++ tests/controllers/UserController.spec.ts
@@ -2,15 +2,9 @@ import * as request from 'supertest';
 import {Response} from 'supertest';
 import app from '../../src/app';
 
-describe("test auth router", () => {
-	test('whoami with no token should return 403', () => {
-		return request(app)
-			.get('/auth/whoami')
-			.then((res: Response) => {
-				expect(res.status).toBe(403);
-			});
-	});
+let jwtTokenAfterSignIn: string;
 
+describe("test auth router", () => {
 	test("create user with no body should return 422", () => {
 		return request(app)
 			.post('/auth/create')
@@ -101,6 +95,32 @@ describe("test auth router", () => {
 						token: expect.anything()
 					}
 				}))
+
+				// we have token, assign to global variable so we can use it for more tests
+				jwtTokenAfterSignIn = res.body.data.token;
 			});
 	})
+
+	test('whoami with no token should return 403', () => {
+		return request(app)
+			.get('/auth/whoami')
+			.then((res: Response) => {
+				expect(res.status).toBe(403);
+			});
+	});
+
+	test('whoami with token should return 200 with success and data.payload', () => {
+		return request(app)
+			.get('/auth/whoami')
+			.set('Authorization', `Bearer: ${jwtTokenAfterSignIn}`)
+			.then((res: Response) => {
+				expect(res.status).toBe(200);
+				expect(res.body).toEqual(expect.objectContaining({
+					success: true,
+					data: {
+						payload: expect.objectContaining({})
+					}
+				}));
+			});
+	});
 });
